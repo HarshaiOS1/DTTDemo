1935f32ae3e2a40853e31eb513ef89c4ac077489
HarshaiOS1 <kharsha21@yahoo.in>
2024-11-12 19:24:21 +0100

    content section rounded in detail page ui fix along with minor code cleanup

--- dtt/lib/screens/house_detail_page.dart
+++ dtt/lib/screens/house_detail_page.dart
@@ -3,6 +3,7 @@ import 'package:dtt/widgets/house_map_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:intl/intl.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 import '../model/house_model.dart';
 import '../services/app_text_styles.dart';
 
@@ -21,69 +22,98 @@ class HouseDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Stack(
+        clipBehavior: Clip.none,
         children: [
-          SingleChildScrollView(
-            child: Column(
-              children: [
-                Image.network(
-                  Constants.baseUrl + house.image,
-                  height: 250.h,
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                ),
-                Padding(
-                  padding: EdgeInsets.all(16.sp),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      /// Displays the price, number of bedrooms, bathrooms, and size of the house.
-                      Row(
-                        children: [
-                          Text('\$${NumberFormat('#,##0').format(house.price)}',
-                              style: AppTextStyles.titleSemiBold20()),
-                          const Spacer(),
-                          Icon(Icons.bed, size: 18.sp),
-                          SizedBox(width: 4.w),
-                          Text('${house.bedrooms}'),
-                          SizedBox(width: 16.w),
-                          Icon(Icons.bathtub, size: 18.sp),
-                          SizedBox(width: 4.w),
-                          Text('${house.bathrooms}'),
-                          SizedBox(width: 16.w),
-                          Icon(Icons.square_foot, size: 18.sp),
-                          SizedBox(width: 4.w),
-                          Text('${house.size} m²'),
-                          SizedBox(width: 16.w),
-                        ],
-                      ),
-                      SizedBox(height: 16.h),
-                      Text(
-                        'Description',
-                        style: AppTextStyles.titleSemiBold18(),
-                      ),
-                      SizedBox(height: 8.h),
-                      Text(
-                        house.description,
-                        style: AppTextStyles.detailLight(),
-                      ),
-                      SizedBox(height: 20.h),
-                      Text(
-                        'Location',
-                        style: AppTextStyles.titleSemiBold18(),
-                      ),
-                      SizedBox(height: 16.h),
-                      HouseMapWidget(
-                        latitude: house.latitude.toDouble(),
-                        longitude: house.longitude.toDouble(),
-                      ),
-                      SizedBox(height: 16.h),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+          Image.network(
+            Constants.baseUrl + house.image,
+            height: 250.h,
+            width: double.infinity,
+            fit: BoxFit.cover,
           ),
+          ScrollablePositionedList.builder(
+              padding: EdgeInsets.fromLTRB(0.0, 240.h, 0.0, 0.0),
+              itemCount: 1,
+              itemBuilder: (context, index) {
+                return Container(
+                  width: MediaQuery.sizeOf(context).width,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: const BorderRadius.only(
+                      topRight: Radius.circular(10),
+                      topLeft: Radius.circular(10),
+                    ),
+                    border: Border.all(
+                      width: 1,
+                      color: Colors.white,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(16.sp),
+                    child: SingleChildScrollView(
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                    '\$${NumberFormat('#,##0').format(house.price)}',
+                                    style: AppTextStyles.titleSemiBold20()),
+                                const Spacer(),
+                                Icon(Icons.bed,
+                                    size: 18.sp, color: Colors.grey),
+                                SizedBox(width: 4.w),
+                                Text(
+                                  '${house.bedrooms}',
+                                  style: const TextStyle(color: Colors.grey),
+                                ),
+                                SizedBox(width: 16.w),
+                                Icon(Icons.bathtub,
+                                    size: 18.sp, color: Colors.grey),
+                                SizedBox(width: 4.w),
+                                Text(
+                                  '${house.bathrooms}',
+                                  style: const TextStyle(color: Colors.grey),
+                                ),
+                                SizedBox(width: 16.w),
+                                Icon(Icons.square_foot,
+                                    size: 18.sp, color: Colors.grey),
+                                SizedBox(width: 4.w),
+                                Text(
+                                  '${house.size} m²',
+                                  style: const TextStyle(color: Colors.grey),
+                                ),
+                                SizedBox(width: 16.w),
+                              ],
+                            ),
+                            SizedBox(height: 16.h),
+                            Text(
+                              'Description',
+                              style: AppTextStyles.titleSemiBold18(),
+                            ),
+                            SizedBox(height: 8.h),
+                            Text(
+                              house.description,
+                              style: AppTextStyles.detailLight(),
+                            ),
+                            SizedBox(height: 20.h),
+                            Text(
+                              'Location',
+                              style: AppTextStyles.titleSemiBold18(),
+                            ),
+                            SizedBox(height: 16.h),
+                            HouseMapWidget(
+                              latitude: house.latitude.toDouble(),
+                              longitude: house.longitude.toDouble(),
+                            ),
+                            SizedBox(height: 30.h),
+                          ]),
+                    ),
+                  ),
+                );
+              }),
           Positioned(
             top: 40.h,
             left: 16.w,
